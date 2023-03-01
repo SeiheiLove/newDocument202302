b4771a23ff518611344292c52c26f7e5cf34442c
SeiheiLove <1092535556@qq.com>
2023-03-01 13:19:45 +0800

    添加pom文件

---
+++ src/main/java/com/example/newdocument202302/NewDocument202302Application.java
@@ -0,0 +1,13 @@
+package com.example.newdocument202302;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewDocument202302Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewDocument202302Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newdocument202302/NewDocument202302ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newdocument202302;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewDocument202302ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
